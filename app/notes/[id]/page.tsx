a6ab13426d9b203037cac2d1c7ff3f420cf197c8
HryhoriyK <k.hryhoriy88@gmail.com>
2025-08-24 19:21:24 +0200

    01

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -4,29 +4,33 @@ import NoteDetailsClient from "./NoteDetails.client";
 import type { Metadata } from "next";
 
 type Props = {
-  params: Promise<{ id: string }>;
+  params: { id: string };
 };
 
-export const metadata: Metadata = {
-  title: "Note | NoteHub",
-  description: "View and edit your notes in NoteHub.",
-  openGraph: {
-    title: "Note | NoteHub",
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const note = await fetchNoteById(params.id);
+  
+  return {
+    title: note.title,
     description: "View and edit your notes in NoteHub.",
-    url: "https://notehub.example.com/notes",
-    images: [
-      {
-        url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
-        width: 1200,
-        height: 630,
-        alt: "NoteHub Note",
-      },
-    ],
-  },
-};
+    openGraph: {
+      title: note.title,
+      description: "View and edit your notes in NoteHub.",
+      url: `https://notehub.example.com/notes/${note.id}`,
+      images: [
+        {
+          url: "[https://ac.goit.global/fullstack/react/notehub-og-meta.jpg](https://ac.goit.global/fullstack/react/notehub-og-meta.jpg)",
+          width: 1200,
+          height: 630,
+          alt: "NoteHub Note",
+        },
+      ],
+    },
+  };
+}
 
 const NoteDetails = async ({ params }: Props) => {
-  const { id } = await params;
+  const { id } = params;
   const queryClient = new QueryClient();
 
   await queryClient.prefetchQuery({
